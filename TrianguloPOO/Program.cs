5f5cd690af28275935a1c6c5941354b1ea7863f7
guilsk <guilherme.gmdp@gmail.com>
2023-04-05 16:57:42 -0300

    Add project files.

---
+++ TrianguloPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TrianguloPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t = new Triangulo();
+            t.lado1 = 3;
+            t.lado2 = 3;
+            t.lado3 = 3;
+            t.ConfereTipo(t.IsTriangle());
+        }
+    }
+}
---
+++ TrianguloPOO/Triangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloPOO
+{
+    internal class Triangulo
+    {
+        public int lado1;
+        public int lado2;
+        public int lado3;
+
+        public bool IsTriangle()
+        {
+            if(lado1 + lado2 <= lado3)
+                return false;
+            else if(lado1 + lado3 <= lado2)
+                return false;
+            else if(lado2 + lado3 <= lado1)
+                return false;
+            else return true;
+        }
+
+        public void ConfereTipo(bool x)
+        {
+            if (x)
+                if (lado1 == lado2 && lado2 == lado3)
+                    Console.WriteLine("Triângulo Equilátero");
+                else if ((lado1 == lado2 && lado3 != lado2) || (lado1 == lado3 && lado2 != lado3) || (lado2 == lado3 && lado1 != lado2))
+                    Console.WriteLine("Triângulo Isóceles");
+                else
+                    Console.WriteLine("Triângulo Escaleno");
+            else
+                Console.WriteLine("Não é Triângulo");
+        }
+    }
+}
